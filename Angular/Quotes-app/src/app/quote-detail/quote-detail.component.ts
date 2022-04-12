c45a24e0161ab387ec3cb2b36fa0803c3c31d59f
Eveline32 <johndoe@example.com>
2022-04-12 12:56:12 +0300

    specify highlight to upvotes greater than zero

--- Angular/Quotes-app/src/app/quote-detail/quote-detail.component.ts
+++ Angular/Quotes-app/src/app/quote-detail/quote-detail.component.ts
@@ -12,6 +12,9 @@ export class QuoteDetailComponent implements OnInit {
     new Quote(1,'Peter', 'First Quote', 'Kofi Anan', new Date(2012, 3, 12), 0,0),
     new Quote(2,'Esther', 'Second Quote', 'Barack Obama', new Date(2013, 5, 13), 0,0),
     new Quote(3,'Getrude', 'Third Quote', 'KNelson Mandela', new Date(2021, 9, 1), 0,0),  ]
+
+    arr:number[] = this.quotes.map(quote=>quote.upVotes)
+    highest =Math.max(...this.arr)
   constructor() { }
 
   ngOnInit(): void {
